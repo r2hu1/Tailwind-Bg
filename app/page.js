89ef5d815aaffb44c1a2fb0619d1dd855e906efb
Rahul Rajput <newasram86@gmail.com>
2024-02-01 16:03:55 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ export default function Page() {
         <h1 className="text-center max-w-md md:max-w-4xl mx-auto text-3xl font-medium text-gray-900 dark:text-gray-50 sm:text-5xl">
           Collection of Stunning <span className="animate-text-gradient inline-flex bg-gradient-to-r from-neutral-900 via-slate-500 to-neutral-500 bg-[200%_auto] bg-clip-text text-transparent dark:from-neutral-100 dark:via-slate-400 dark:to-neutral-400 leading-tight">Backgrounds and Gradients.</span>
         </h1>
-        <p className="text-center leading-tight mt-2 mx-auto max-w-sm md:max-w-xl text-sm text-gray-600 dark:text-gray-200">Ready-to-use, just copy and paste <span className="opacity-70">className</span> into your code. crafted with <span className="opacity-70">Tailwind CSS</span> tweak it to your taste.</p>
+        <p className="text-center leading-tight mt-2 mx-auto max-w-xs md:max-w-xl text-sm text-gray-600 dark:text-gray-200">Ready-to-use, just copy and paste <span className="opacity-70">className</span> into your code. crafted with <span className="opacity-70">Tailwind CSS</span> tweak it to your taste.</p>
         <div className="mt-7 flex gap-2 items-center justify-center">
           <Button asChild><Link target="_blank" href="https://github.com/r2hu1/tailwind-bg">View Source</Link></Button>
           <Reset />
